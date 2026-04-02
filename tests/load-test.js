dcea7344a1c568a9a179720e3c0d2f21d327f1ee
AnaGabrielaMaia <agmlima2001@gmail.com>
2026-04-02 18:21:34 -0300

    corrige testes

--- tests/load-test.js
+++ tests/load-test.js
@@ -5,8 +5,8 @@ import { check, sleep } from "k6";
 /*export const options = {
   stages: [
     { duration: "30s", target: 50 }, // sobe para 50 usuários
-    { duration: "1m", target: 200 }, // sobe para 200 usuários
-    { duration: "2m", target: 500 }, // pico de carga
+    { duration: "30s", target: 200 }, // sobe para 200 usuários
+    { duration: "30s", target: 500 }, // pico de carga
     { duration: "30s", target: 0 }, // reduz
   ],
 };*/
@@ -17,12 +17,35 @@ export const options = {
   duration: "2m",
 };
 
+const videoIds = [
+  "f1aa0385-bd31-4c8f-8029-3ef328d7ca33",
+  "2aac43cf-6619-4e27-be13-93253b325aa6",
+  "cf38723f-6f01-4374-be95-2f085ed22642",
+];
+
 export default function () {
-  const res = http.get("http://localhost:3000/public");
+  const videoId = videoIds[Math.floor(Math.random() * videoIds.length)];
+  const url = `http://127.0.0.1:4000/videos/${videoId}/challenges`;
+
+  const res = http.post(url, null, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  let body;
+
+  try {
+    body = JSON.parse(res.body);
+  } catch (e) {
+    body = {};
+  }
 
   check(res, {
     "status 200": (r) => r.status === 200,
     "response time < 500ms": (r) => r.timings.duration < 500,
+    "tem pergunta": () => body.question && body.question.length > 10,
+    "não é fallback": () => body.source !== "static",
   });
 
   sleep(1);
